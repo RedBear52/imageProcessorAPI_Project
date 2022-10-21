866e1e9d6906f328ac21fe43be837ec9dcad9ca5
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2022-10-21 12:03:09 -0500

    Added and Configured: eslint and prettier

---
+++ src/modules/_grayscale.ts
@@ -0,0 +1,9 @@
+import sharp from 'sharp'
+
+const convertToGrayScale = () => {
+    sharp('images/happel.jpg')
+        .grayscale()
+        .toFile('processed_images/grayscale_happel.jpg')
+}
+
+convertToGrayScale()
---
+++ src/server/server.ts
@@ -0,0 +1,37 @@
+import sharp from 'sharp'
+
+const express = require('express')
+const app = express()
+
+app.get(
+    '/api/resize',
+    async (
+        req: { query: { filename: any; width: any; height: any } },
+        res: {
+            type: (arg0: string) => {
+                (): any
+                new (): any
+                send: { (arg0: Buffer): void; new (): any }
+            }
+        }
+    ) => {
+        const filename = req.query.filename
+        const width = req.query.width
+        const height = req.query.height
+
+        sharp(`./images/${filename}.jpg`)
+            .resize(parseInt(width), parseInt(height), {
+                fit: sharp.fit.inside,
+                withoutEnlargement: false,
+            })
+            .toBuffer()
+            .then((data) => {
+                res.type('jpg').send(data)
+            })
+    }
+)
+
+const port = 3200
+app.listen(port, () => {
+    console.log(`server awake and listening at: ${port}`)
+})
