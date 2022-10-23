ce0a6df23e681faa9f7a18861232a17ddff6b737
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2022-10-23 09:51:50 -0500

    Now handling all predicted errors EXCEPT a filename entered as string that references a non-existent file | Afforementioned scenario crashes app

--- src/routes/middleware/validateInput.ts
+++ src/routes/middleware/validateInput.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { isNumber } from 'util'
 
 const validateInput = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
     if (!(req.query.filename || req.query.width || req.query.height)) {
@@ -10,6 +11,11 @@ const validateInput = (req: express.Request, res: express.Response, next: expres
     }  else if (isNaN(parseInt(`${req.query.height}`))) {
         res.status(400)
         next('Error: Your query string is missing a proper HEIGHT parameter - Please enter a numeric value')
+    }  else if (!req.query.filename || parseInt(`${req.query.filename}`)) {
+        res.status(400)
+        next(`Error: Your query string is missing a proper FILENAME parameter.
+        Please enter a filename that exists and make sure to use a string to do so.
+        Numerical values will not work.`)
     }  else {
         next()
     }
