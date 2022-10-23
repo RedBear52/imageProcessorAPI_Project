e828cd865421cd48397e262c8164a440a1cbf2cb
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2022-10-23 10:01:26 -0500

    Seem to have all likely input errors handled via validateInput middleware Module

--- src/routes/middleware/validateInput.ts
+++ src/routes/middleware/validateInput.ts
@@ -1,23 +1,29 @@
 import express from 'express'
-import { isNumber } from 'util'
+import fs from 'fs'
+import path from 'path'
 
 const validateInput = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+    const imgPath = path.resolve(`./images/${req.query.filename}.jpg`)
+
     if (!(req.query.filename || req.query.width || req.query.height)) {
         res.status(400)
         next('Error: Your query string must include filename, width & height parameters')
     } else if (isNaN(parseInt(`${req.query.width}`))) {
         res.status(400)
-        next('Error: Your query string is missing a proper WIDTH parameter - Please enter a numeric value')
+        next('Error: Your query string is missing a proper WIDTH parameter - Please enter a numerical value')
     }  else if (isNaN(parseInt(`${req.query.height}`))) {
         res.status(400)
-        next('Error: Your query string is missing a proper HEIGHT parameter - Please enter a numeric value')
+        next('Error: Your query string is missing a proper HEIGHT parameter - Please enter a numerical value')
     }  else if (!req.query.filename || parseInt(`${req.query.filename}`)) {
         res.status(400)
         next(`Error: Your query string is missing a proper FILENAME parameter.
         Please enter a filename that exists and make sure to use a string to do so.
-        Numerical values will not work.`)
-    }  else {
+        HINT: Numerical values will not work.`)
+    }  else if (fs.existsSync(imgPath)) {
         next()
+    }  else {
+        res.status(400)
+        next('Error: Filename is invalid. Please choose an extant file, make sure it is spelled correctly and make sure it is a STRING.')
     }
 }
 
