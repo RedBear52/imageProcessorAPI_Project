614a35e8a1b7ffc48a36aed62454d336965198d2
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2022-10-23 09:39:22 -0500

    Handled NaN width and height errors | Working on no filename  scenario (not as straightforward as hoped)

--- src/routes/middleware/validateInput.ts
+++ src/routes/middleware/validateInput.ts
@@ -4,7 +4,13 @@ const validateInput = (req: express.Request, res: express.Response, next: expres
     if (!(req.query.filename || req.query.width || req.query.height)) {
         res.status(400)
         next('Error: Your query string must include filename, width & height parameters')
-    } else {
+    } else if (isNaN(parseInt(`${req.query.width}`))) {
+        res.status(400)
+        next('Error: Your query string is missing a proper WIDTH parameter - Please enter a numeric value')
+    }  else if (isNaN(parseInt(`${req.query.height}`))) {
+        res.status(400)
+        next('Error: Your query string is missing a proper HEIGHT parameter - Please enter a numeric value')
+    }  else {
         next()
     }
 }
